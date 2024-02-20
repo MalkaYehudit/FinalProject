1d863e8561199798b88d362558bf9acc20d6edf6
טאוב רינה < @mbjcomp.local>
2024-02-20 18:15:08 +0200

    add professionals controller

---
+++ EmploymentDirectory/EmploymentDirectory/Controllers/ProfessionalsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmploymentDirectory.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfessionalsController : ControllerBase
+    {
+    }
+}
